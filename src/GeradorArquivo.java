e82398772d9ba84fd366df564a83f356b2b973a5
João Pedro <jplmarotinho22@gmail.com>
2025-10-27 12:04:30 -0300

    Projeto finalizado.

--- src/GeradorArquivo.java
+++ src/GeradorArquivo.java
@@ -3,13 +3,16 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class GeradorArquivo {
-    public void geraArquivo(Endereco endereco) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public void geraArquivo(List<Endereco> enderecos) throws IOException {
+        for(Endereco endereco : enderecos) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        FileWriter escrita = new FileWriter(endereco.cep() + ".json");
-        escrita.write(gson.toJson(endereco));
-        escrita.close();
+            FileWriter escrita = new FileWriter(endereco.cep() + ".json");
+            escrita.write(gson.toJson(endereco));
+            escrita.close();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ConsultaCep consulta = new ConsultaCep();
+        String cep = "";
+        List<Endereco> enderecos = new ArrayList<>();
 
-        System.out.println("Digite o número do CEP: ");
-        var cep = scan.nextLine();
-        try {
-            Endereco endereco = consulta.buscaEndereco(cep);
-            System.out.println(endereco);
-            GeradorArquivo gerador = new GeradorArquivo();
-            gerador.geraArquivo(endereco);
-        } catch (RuntimeException | IOException e) {
-            System.out.println(e.getMessage());
+        while(!cep.equalsIgnoreCase("sair")) {
+            System.out.println("Digite o número do CEP, ou a palavra 'sair': ");
+            cep = scan.nextLine();
+
+            if(cep.equalsIgnoreCase("sair")) {
+                System.out.println("Execução interrompida pelo usuário.");
+                break;
+            }
+
+            try {
+                Endereco endereco = consulta.buscaEndereco(cep);
+                System.out.println(endereco);
+                enderecos.add(endereco);
+            } catch (RuntimeException e) {
+                System.out.println("Erro em tempo de execução: ");
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if(!enderecos.isEmpty()) {
+            GeradorArquivo geradorArquivo = new GeradorArquivo();
+            try {
+                geradorArquivo.geraArquivo(enderecos);
+            } catch (IOException e) {
+                System.out.println("Erro ao salvar arquivos: ");
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
